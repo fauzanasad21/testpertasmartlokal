103f0954e2d60c3ca5ca896d2f92aeeeb3629da0
kameel48 <164047263+kameel48@users.noreply.github.com>
2025-08-01 18:09:45 +0700

    Delete Dashboard directory

--- Dashboard/Backeund_Node/config/auth.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-
-const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
-
-module.exports = {
-    jwt, JWT_SECRET, JWT_REFRESH_SECRET
-}
